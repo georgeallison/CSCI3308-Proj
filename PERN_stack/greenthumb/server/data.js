b19e55d509ab47965f2c40e7b2a8777bece29877
George Allison <george@Georges-MacBook-Pro.local>
2019-03-23 13:12:51 -0600

    trying our PERN stack configuration, will commit README soon

---
+++ PERN_stack/greenthumb/server/data.js
@@ -0,0 +1,7 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const DataSchema = new Schema(
+	{id: Number, message: String},
+	{timestamps: true}
+);
+module.exports = mongoose.model("Data", DataSchema);
